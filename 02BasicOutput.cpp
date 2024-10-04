70640e93452bffe77f1e829d6fe13c108798534e
John Rey Elep <jelep76@gmail.com>
2024-10-04 09:01:41 +0800

    Add files via upload
    PROGRAMMING ACTIVITY 02

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << " School: Our Lady of Lourdes College Foundation " << endl;
+	cout << " Department: College of Computer Studies " << endl; 
+	cout << " Course & Year Level: Bachelor of Science in Information and Technology year 1, block C " << endl;
+	cout << " Full name: John Rey Icaro Elep " << endl;
+	cout << " Permanent Address: Purok-5, Lubhag Calangcawan Sur, Vinzons Camarines norte. " << endl;
+	
+	return 0;
+	
+}
