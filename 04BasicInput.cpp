e0fcbc668b8d017569bbc8c34a13cc87e63fc29a
John Rey Elep <jelep76@gmail.com>
2024-10-04 09:16:12 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << " Nickname: Toton " << endl; 
+	cout << " Course-Year Level: BSIT 1, Block C" << endl;
+	cout << " School: Our Lady of Lourdes College Foundation " << endl;
+	
+	return 0;
+	
+}
