21e2c4778541478188fcfadbff9e6d1a0a2deec1
John Rey Elep <jelep76@gmail.com>
2024-10-08 15:07:21 +0800

    Add files via upload
    PROGRAMMING 05

---
+++ 05Converts_USD_to_Peso.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main ()
+{
+	double Dollar;
+	double Peso;
+	
+	// get amount in USD
+	cout << "Enter the amount of dollar\n";
+	cin >>Dollar;
+	
+	// Calculate the peso amount
+	Peso = 56.83 * Dollar;
+	
+	//Convert result 
+	cout << "USD: " << Dollar << "\t\t" << " PhP: " << Peso << endl;
+	
+}
