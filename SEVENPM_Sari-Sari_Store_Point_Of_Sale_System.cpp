1df16b7ab9adb5388ad1a672f6f9f5aa18bdb653
John Rey Elep <jelep76@gmail.com>
2024-10-14 10:16:03 +0800

    Add files via upload
    Hands-on-exam

---
+++ SEVENPM_Sari-Sari_Store_Point_Of_Sale_System.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	float sugarPriceUSD = 21.14, ricePricePound = 24.13, sardinesPricePound = 47.07,  coffeePriceUSD = 5.58, milkPriceUSD = 4.021, pound = 73.74, PHP = 0;
+	float usdToPhp, poundToPhp, totalCostPhp;
+	int sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	
+	const float USD = 56;
+	cout << "Enter the quantity of sugar\n";
+	cin >> sugarQty;
+	cout << "Enter the quantity of rice\n";
+	cin >> riceQty;
+	cout << "Enter the quantity of sardines\n";
+	cin >> sardinesQty;
+	cout << "Enter the quantity of coffee\n";
+	cin >> coffeeQty;
+	cout << "Enter the quantity of milk\n";
+	cin >> milkQty;
+	cout << "\n";
+	
+	//Currency COnversion
+	usdToPhp =  USD * PHP;
+	poundToPhp = pound * PHP;
+	totalCostPhp = 
+	cout << "\n";
+	
+	cout << "Price of sugar in PHP\n";
+	cin >> ?;
+	cout << "Price of rice in PHP\n";
+	cin >> ?;
+	cout << "Price of sardines in PHP\n";
+	cin >> ?;
+	cout << "Price of coffee in php\n";
+	
+	
+	
+	cout << "The total cost";
+	cin >> totalCostPhp;
+	
+	return 0;
+	
+}
